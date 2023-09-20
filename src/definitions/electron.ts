0e8196af9ba19804eed1bc054b3f91c16ac650f0
“Mr <umar.b@mblhightech.com>
2023-09-20 18:01:11 +0300

    Added reponse from main process

---
+++ src/definitions/electron.ts
@@ -0,0 +1,3 @@
+export const IPCKeys = {
+  getCompletion: "getCompletion",
+} as const;
--- src/preload.ts
+++ src/preload.ts
@@ -1,12 +1,14 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
 
-import { contextBridge } from "electron";
+import { contextBridge, ipcRenderer } from "electron";
+import { IPCKeys } from "./definitions/electron";
 
 contextBridge.exposeInMainWorld("electron", {
   chatgptApi: {
-    getCompletion(prompt: string) {
-      return prompt;
+    async getCompletion(prompt: string) {
+      const res = await ipcRenderer.invoke(IPCKeys.getCompletion, prompt);
+      return res;
     },
   },
 });
